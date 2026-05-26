1a67b11cd262c101531f73c9c53187ac389d9d52
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2026-05-26 04:56:19 -0400

    Double-checked up through mid Ch 6

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,7 @@
-window.document.addEventListener("DOMContentLoaded", function(event) {
-    var subtitleHtml = "<span class='sidebar-subtitle'>DSAN 5650 Edition</span>";
-    var subtitleDiv = $(`<br>${subtitleHtml}`);
-    $('.sidebar-title').append(subtitleDiv);
-});
+window.document.addEventListener(
+    "DOMContentLoaded",
+    function(event) {
+        const subtitleHtml = `<span class='sidebar-subtitle'>DSAN 5650 Edition</span>`
+        document.querySelector('.sidebar-title').innerHTML += `<br>${subtitleHtml}`
+    }
+)
